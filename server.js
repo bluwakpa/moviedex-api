99858d062f9f81def3e2c19ada4be677b38bd852
Makha Blu Wakpa <bluwakpa@Makhas-MacBook-Pro.local>
2021-02-06 16:52:44 -0800

    heroku

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const helmet = require('helmet')
 const MOVIES = require('./movies-data-small.json')
 //
 const app = express()
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
 
-app.use(morgan('dev'))
+app.use(morgan(morganSetting))
 app.use(cors())
 app.use(helmet())
 
@@ -44,7 +45,18 @@ app.get('/movie', function handleGetMovie(req, res) {
   res.json(response)
 })
 
-const PORT = 9000
+// 4 parameters in middleware, express knows to treat this as error handler
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 9000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
